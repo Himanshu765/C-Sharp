927f1e881e05c522d76d43a7861d9ef99c11b2ff
Himanshu chauhan <101649802+Himanshu765@users.noreply.github.com>
2023-03-29 16:53:19 +0530

    Update chain.cs

--- chain.cs
+++ chain.cs
@@ -214,20 +214,200 @@ class chetu
 {
 	static void Main()
 	{
-			Employee emp = new Employee(101," Himanshu", address);
+	
+			
 			Address address = new Address();
+			
+			Employee emp = new Employee(101," Himanshu", address);
+			
+		    address.Houseno=101;
+		    address.Housename= "chauhanniwas";
+		    address.Pincode= 246746;
+			
+			Console.WriteLine("emp no is:"+emp.EmpNo);
+			Console.WriteLine("emp name is:"+emp.EmpName);
+			Console.WriteLine("emp address is:"+emp.EmpAddress);
 		
-		address.Houseno=101;
-		address.Housename=" chauhanniwas";
-		address.Pincode=246746;
-		Console.WriteLine(" House no  is  "+address.Houseno);
-		Console.WriteLine(" House name  is  "+address.Housename);
-		Console.WriteLine(" House no  is  "+address.Pincode);
 	}
 }
 */
 
+//*********************************************************************
 
+/*
+
+class Address 
+		// Third  METHOD USING Constructor   USER DEFINED DATA TYPE 
+{
+	// using Constructor 
+	
+	private int  _houseno;
+	private  string  _housename;
+	private int  _pincode;
+	
+	public Address(int houseno, string housename , int pincode)
+	{
+		_houseno = houseno;
+		_housename = housename;
+		_pincode = pincode;
+	}
+	
+	public int HouseNo
+	{
+		get{return _houseno;}
+		
+	}
+	public string  HouseName
+	{
+		get{return _housename;}
+	}
+		
+	public int Pincode
+	{
+		get{return _pincode;}
+		
+	}
+	
+	
+	public override string ToString()
+	{
+		{
+		 return  HouseNo +"  " + HouseName +"  "+Pincode;
+		}
+	}
+}
+	
+class Employee
+{
+	// using Constructor
+	
+		private int _empno;
+		private string  _empname;
+		private Address _empaddress;
+		
+	public Employee ( int empno, string empname,  Address empaddress)
+	{
+		_empno=empno;
+		_empname=empname;
+		_empaddress = empaddress;
+	
+	}
+	
+	public int EmpNo
+	{
+		get{return _empno;}
+		
+	}
+	public string  EmpName
+	{
+		get{return _empname;}
+	}
+		
+	public Address EmpAddress
+	{
+		get{return _empaddress;}
+		
+	}
+	
+		
+}
+class chetu
+{
+	static void Main()
+	{
+	
+			
+			Address address = new Address(101," rajvilla",246746);
+			
+			Employee emp = new Employee(101," Himanshu", address);
+			
+		    			
+			Console.WriteLine("emp no is:"+emp.EmpNo);
+			Console.WriteLine("emp name is:"+emp.EmpName);
+			Console.WriteLine("emp address is:"+emp.EmpAddress);
+		
+	}
+}
+*/
+
+//*****************************************************************************************
+
+class Address 
+
+		// fourth  METHOD USING  GET & SET PROPERTY USER DEFINED DATA TYPE 
+{
+	// using property 
+	
+	private int  _houseno;
+	private  string  _housename;
+	private int  _pincode;
+	
+	public int Houseno
+	{
+		get; set;
+	} 
+	public string Housename
+		{
+		get; set;
+		} 	 
+		public int Pincode
+	{
+		get; set;
+	}
+
+	public override string ToString()
+	{
+		{
+		 return  Houseno +"  " + Housename +"  "+Pincode;
+		}
+	}
+}	
+class Employee
+{
+		private int _empno;
+		private string  _empname;
+		private Address _empaddress;
+		
+	public int Empno
+	{
+		get;set;
+	}
+	
+	public string  Empname
+	{
+		get;set;
+	}
+	
+	public Address  Empaddress
+ 	{
+		get;set;
+	}
+		
+}
+
+class Test
+{
+	static void Main()
+	{
+		Address address = new Address();
+		Employee emp = new Employee();
+		
+		emp.Empno=101;
+		emp.Empname=" Himanshu";
+		emp.Empaddress=address;
+		
+		address.Houseno=100;
+		address.Housename=" Raj Villa";
+		address.Pincode=246746;
+		
+		Console.WriteLine("emp no is:"+emp.Empno);
+		Console.WriteLine("emp name is:"+emp.Empname);
+		Console.WriteLine("emp address is:"+emp.Empaddress);
+		
+	}
+}
+
+/*
 class student 
 {
 	private  int  _studentId;
@@ -256,3 +436,11 @@ class Test
 		
 	}
 }
+*/
+
+
+
+
+
+
+
