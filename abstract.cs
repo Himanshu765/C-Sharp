c31f82fdc9a213c4db9ce9bdca9ee2af625b0410
Himanshu chauhan <101649802+Himanshu765@users.noreply.github.com>
2023-03-30 10:51:19 +0530

    Add files via upload

---
+++ abstract.cs
@@ -0,0 +1,155 @@
+using System;
+/*
+		ABSTRACT CLASS 
+		*****************************
+		
+ abstract class RBI
+{
+	public  virtual void interest()
+	{
+		Console.WriteLine(" 10%");
+	}
+}
+
+class HDFC:RBI
+{
+	
+}
+
+class ICICI:RBI
+{
+	public override void interest()
+	{
+		Console.WriteLine(" 5%");
+	}
+}
+
+class Customer
+{
+	static void Main()
+	{
+		RBI obj= new HDFC();
+		obj.interest();
+		RBI obj1= new ICICI();
+			obj1.interest();
+	}
+}
+*/
+
+/*
+		abstract method 
+********************************
+
+abstract class RBI
+{
+	public abstract   void interest();
+	
+}
+
+class HDFC:RBI
+{
+	public override  void interest()
+	{
+		Console.WriteLine(" 10%");
+	}
+}
+
+class ICICI:RBI
+{
+	public override void interest()
+	{
+		Console.WriteLine(" 5%");
+	}
+}
+
+class Customer
+{
+	static void Main()
+	{
+		RBI obj= new HDFC();
+		obj.interest();
+		RBI obj1= new ICICI();
+		obj1.interest();
+	}
+}
+*/
+
+/*
+		abstract class using constructor 
+		************************************
+		
+abstract class RBI
+{
+	public abstract   void interest();
+	public RBI()
+	{
+		Console.WriteLine("  Constructor hai  ");
+	}
+	
+}
+
+class HDFC:RBI
+{
+	public override  void interest()
+	{
+		Console.WriteLine(" 10%");
+	}
+}
+
+class ICICI:RBI
+{
+	public override void interest()
+	{
+		Console.WriteLine(" 5%");
+	}
+}
+
+class Customer
+{
+	static void Main()
+	{
+		RBI obj= new HDFC();
+		obj.interest();
+		RBI obj1= new ICICI();
+		obj1.interest();
+	}
+}
+*/
+
+abstract class RBI
+{
+	public abstract   void interest();
+	public RBI()
+	{
+		Console.WriteLine("  Constructor hai  ");
+	}
+	static void Main ()
+	{
+		Console.WriteLine("  I am a RBI Bank");
+		
+		RBI obj= new HDFC();
+		RBI obj1= new ICICI();
+		obj.interest();
+		obj1.interest();
+	
+	}
+}
+
+class HDFC:RBI
+{
+	public override  void interest()
+	{
+		Console.WriteLine(" 10%");
+	}
+}
+
+class ICICI:RBI
+{
+	public override void interest()
+	{
+		Console.WriteLine(" 5%");
+	}
+}
+
+
+
