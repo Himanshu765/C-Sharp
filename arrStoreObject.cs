048c01c8993a048ba3ff1b1d50c17d45fa956f23
Himanshu chauhan <101649802+Himanshu765@users.noreply.github.com>
2023-04-24 12:38:57 +0530

    Add files via upload

---
+++ arrStoreObject.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Linq;
+
+class StoreValueOther
+{
+	static void Main()
+	{
+		int[] arr={1,2,3,4,8,9,56};
+		Console.Write(arr.Max());
+			/* string name="satishabhi@mail.com";
+			string newstr="";
+			foreach(char my in name)
+			{
+				if((my>='a' && my<='z')||(my>='0' &&my<='9')|| my==' ')
+				{
+					newstr+=my;
+				}
+			}*/
+		// Full name
+			    
+	}
+}
